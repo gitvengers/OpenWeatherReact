bc55298a9dbbb32a6a641aa1a9a070ca495fcae4
Alencion <rhqnrdl625@naver.com>
2019-05-31 22:12:58 +0900

    modify to show title 'Cities list' when Weather list is loading.

--- src/Component/Weather/index.js
+++ src/Component/Weather/index.js
@@ -7,33 +7,40 @@ import './Weather.css';
 
 const API_CITIES = "http://localhost:8080/weather-crawler/available-cities";
 
-class Weather extends  React.Component{
-    state ={
-        cities : null
+class Weather extends React.Component {
+    state = {
+        cities: null
     };
+
     async componentDidMount() {
         const cities = await fetch(API_CITIES)
             .then(res => res.json())
-            .then(data =>data);
+            .then(data => data);
 
         this.setState({
             cities
         });
     }
 
-    render(){
+    render() {
         const {match} = this.props;
         const {cities} = this.state;
-        if(!cities){
-            return <div className="loading">loading...</div>
+        if (!cities) {
+            return (
+                <div className="loading">
+                    <h1 className="listHeader"> Cities List </h1>
+                    loading...
+                </div>
+            );
         }
         return (
             <Switch>
                 <Route path={`${match.path}/:cityId`} component={todayWeather}></Route>
-                <Route exact path ={match.path}
-                       render={()=> <List cities={cities} />} />
+                <Route exact path={match.path}
+                       render={() => <List cities={cities}/>}/>
             </Switch>
         );
     }
 }
+
 export default withRouter(Weather);
